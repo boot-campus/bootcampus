da609f7747627035dc06a6b92a25cbafba8b33de
zillionme <beatus96@naver.com>
2024-01-09 05:37:09 +0900

    feat: 로그인 및 회원가입 기능 추가

--- backend/src/main/java/com/boot/campus/auth/application/AuthService.java
+++ backend/src/main/java/com/boot/campus/auth/application/AuthService.java
@@ -1,14 +1,30 @@
 package com.boot.campus.auth.application;
 
 import com.boot.campus.auth.application.dto.GitHubLoginCommand;
+import com.boot.campus.auth.infrastructure.github.GitHubManager;
+import com.boot.campus.member.domain.Member;
+import com.boot.campus.member.persistence.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-
-    private static final String URL = "https://github.com/login/oauth/access_token";
-
-    public void githubLogin(final GitHubLoginCommand command) {
-
+    
+    private final GitHubManager gitHubManager;
+    private final MemberRepository memberRepository;
+    
+    public AuthService(final GitHubManager gitHubManager, final MemberRepository memberRepository) {
+        this.gitHubManager = gitHubManager;
+        this.memberRepository = memberRepository;
+    }
+    
+    public Long login(final GitHubLoginCommand command) {
+        Member member = gitHubManager.login(command.code());
+        Member registerdMember = memberRepository.findByMemberIdentity(member.getMemberIdentity())
+                .orElseGet(()-> register(member));
+        return registerdMember.getId();
+    }
+    
+    private Member register(Member member) {
+       return memberRepository.save(member);
     }
 }
