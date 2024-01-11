1efcaf2509961157c8a1d8e83486d359dab27aa7
zillionme <beatus96@naver.com>
2024-01-11 16:35:36 +0900

    chore: LoginCommand클래스 이름 변경

---
+++ backend/src/main/java/com/boot/campus/auth/application/dto/LoginCommand.java
@@ -0,0 +1,8 @@
+package com.boot.campus.auth.application.dto;
+
+public record LoginCommand(String code) {
+
+    public static LoginCommand from(final String code) {
+        return new LoginCommand(code);
+    }
+}
