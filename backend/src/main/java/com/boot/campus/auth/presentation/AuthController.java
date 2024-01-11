bc7a714e9326388582d72cbc80c9fa18a797fec6
zillionme <beatus96@naver.com>
2024-01-11 16:46:36 +0900

    refactor: 로그인 기능 상태코드 변경

--- backend/src/main/java/com/boot/campus/auth/presentation/AuthController.java
+++ backend/src/main/java/com/boot/campus/auth/presentation/AuthController.java
@@ -3,6 +3,7 @@ package com.boot.campus.auth.presentation;
 import com.boot.campus.auth.application.AuthService;
 import com.boot.campus.auth.application.dto.LoginCommand;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    
-    private static final String SESSION_VALUE = "ID";
     
     private final AuthService authService;
     
@@ -24,8 +23,8 @@ public class AuthController {
                                             final HttpSession httpSession) {
         final LoginCommand command = LoginCommand.from(code);
         final Long memberId = authService.login(command);
-        httpSession.setAttribute(SESSION_VALUE, memberId);
-        httpSession.getAttribute(SESSION_VALUE);
-        return ResponseEntity.ok().build();
+        httpSession.setAttribute("MEMBER_ID", memberId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .build();
     }
 }
