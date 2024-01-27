c20e4861e4431dbc3b71c4e1abd5e025d154e186
zillionme <beatus96@naver.com>
2024-01-27 15:29:04 +0900

    feat: MemberType enum 예외처리 추가

--- backend/src/main/java/com/boot/campus/member/domain/MemberType.java
+++ backend/src/main/java/com/boot/campus/member/domain/MemberType.java
@@ -6,6 +6,10 @@ public enum MemberType {
     ;
     
     public static MemberType fromName(String name) {
-        return MemberType.valueOf(name);
+        try{
+            return MemberType.valueOf(name);
+        }catch (IllegalArgumentException e) {
+            throw new RuntimeException("일치하는 멤버 타입이 존재하지 않습니다");
+        }
     }
 }
