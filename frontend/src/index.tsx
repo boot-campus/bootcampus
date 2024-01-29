299198d4ec6248f16bf517bcb83871f19ab0a106
WizardRabbit <curious.wzrabbit@gmail.com>
2024-01-29 22:03:32 +0900

    chore: GlobalStyle과 theme를 App에서 사용할 수 있도록 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import GlobalStyle from '~styles/GlobalStyle';
+import { theme } from '~styles/theme';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
 );
