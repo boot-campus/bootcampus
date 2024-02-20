624ca95952df884b0d6969ec98576c4e2498f55b
요술토끼 <curious.wzrabbit@gmail.com>
2024-02-20 13:26:41 +0900

    Revert "chore: 프론트엔드 프로젝트의 기본 개발 설정을 진행한다."

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import ColoredTitle from '~components/test/ColoredTitle';
-
-const App = () => {
-  return <ColoredTitle color="teal">Hello, BootCampus!</ColoredTitle>;
-};
-
-export default App;
--- frontend/src/components/test/ColoredTitle/ColoredTitle.styled.ts
+++
@@ -1,6 +0,0 @@
-import { styled } from 'styled-components';
-
-export const ColoredTitle = styled.h1<{ $color: 'black' | 'teal' }>`
-  color: ${({ theme, $color }) =>
-    $color === 'teal' ? theme.color.TEAL : 'black'};
-`;
